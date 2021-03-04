dcdb0e4875bf959858037f9436442a42a3869cd9
Antlers <410111055@qq.com>
2021-03-04 18:43:20 +0800

    2021/03/04 周四 18:43:20.48

--- GitPush.bat
+++ GitPush.bat
@@ -1,3 +1,18 @@
-git add .
-git commit -m "beifen"
+echo "Start submitting code to the local repository"
+echo "The current directory is：%cd%"
+git add *
+echo;
+
+
+echo "Commit the changes to the local repository"
+echo "please enter the commit info...."
+set /p message=
+set now=%date% %time%
+echo %now%
+git commit -m "%now% %message%"
+echo;
+
 git push origin hexo
+
+echo "Batch execution complete!"
+echo;
